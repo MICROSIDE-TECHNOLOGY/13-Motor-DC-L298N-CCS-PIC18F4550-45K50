0096a3094a96c4cb38ee1fa4ea89e58d020fd3bf
vhperaltaj <victor.peralta@microside.com>
2023-08-29 12:24:15 -0600

    Added header & main.yml

--- Proyecto/PIC18F45K50/Motor DC + Driver L298N.c
+++ Proyecto/PIC18F45K50/Motor DC + Driver L298N.c
@@ -1,8 +1,10 @@
-/*
-AUTOR: MICROSIDE TECHNOLOGY S.A. DE C.V.
-FECHA: JUNIO 2019
-*/
-
+/************************************************************************************************
+Company:
+Microside Technology Inc.
+Product Revision  :  1
+Device            :  X-TRAINER
+Driver Version    :  1.0
+************************************************************************************************/
 /*
 ---------------------------------------------------------------------------
 Esta práctica consiste en variar la velocidad de un motor usando el módulo
